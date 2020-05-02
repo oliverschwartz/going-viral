a7b3eb02de63b6918b49d6f6888f8a900a61286b
Oliver Schwartz <ollieschwartz99@gmail.com>
2020-05-02 17:58:27 +1000

    reconfigured gltf according to piazza post @498

--- src/components/objects/Flower/Flower.js
+++ src/components/objects/Flower/Flower.js
@@ -1,7 +1,7 @@
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
-// import MODEL from './flowerOG.gltf';
+import MODEL from './flower.gltf';
 
 class Flower extends Group {
     constructor(parent) {
@@ -20,7 +20,7 @@ class Flower extends Group {
         const loader = new GLTFLoader();
 
         this.name = 'flower';
-        loader.load('./flower.glb', (gltf) => {
+        loader.load(MODEL, (gltf) => {
             this.add(gltf.scene);
         });
 
